e40322dc8096be43fd461238ac7951ad1b6a3cda
vivan123456789 <vivanvaidya@icloud.com>
2025-07-23 12:12:18 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import PleasantonDowntownCatalog from './App';
-import './index.css';
-import 'leaflet/dist/leaflet.css';
+import './index.css'; // optional
+import 'leaflet/dist/leaflet.css'; // critical for map tiles
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
