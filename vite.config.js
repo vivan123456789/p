abc4e40809680ab4fd6eea8535a315b04dafbfd2
vivan123456789 <vivanvaidya@icloud.com>
2025-07-23 12:15:27 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Replace 'pleasanton-downtown' with your repo name
 export default defineConfig({
-  base: '/p/', // <-- Replace with your GitHub repo name
-  plugins: [react()],
+  base: '/p/',
+  plugins: [react()]
 })
